0c1179fbc2d96cb34e55e48c7d09cd5cae0290e5
Yamil Rodriguez Padron <yamil.rodriguez.padron.sa@everis.com>
2019-12-10 12:19:03 +0100

    creating project

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+var mqtt = require("mqtt")
+
+const channel = "ravis/influx"
+const clientMQTT = mqtt.connect("mqtt://18.202.41.95")
+
+clientMQTT.on("connect", function () {
+  // console.log("Connection done!")
+  clientMQTT.subscribe(channel, function (err) {
+    clientMQTT.publish(channel, "hello yamil")
+  })
+})
+ 
+clientMQTT.on("message", function (topic, message) {
+  console.log("from subscriber", topic)
+  console.log(topic, message.toString())
+  // c.end()
+})
+
+/* GET home page. */
+// router.get("/", function(req, res, next) { 
+//   res.render("index", { title: "Express" });
+// });
+
+/* POST home page. */
+router.post("/", function(req, res, next) {
+  /* stablishing connection to mqtt */
+
+  clientMQTT.publish(channel, "hello yamil")
+
+  console.log("req.body", req.body)
+  console.log("req.query", req.query)
+  res.send("ok")
+})
+
+module.exports = router
